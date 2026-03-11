4a153d9fe13692acef5985648a3d54f6c0a906da
Zura Shubitidze <157276781+ZuShubitidze@users.noreply.github.com>
2026-03-11 15:33:37 +0400

    Message

--- Frontend/src/pages/PostsPage.tsx
+++ Frontend/src/pages/PostsPage.tsx
@@ -1,57 +1,45 @@
-import { getPosts } from "@/components/hooks/getPosts";
 import { useInfinitePosts } from "@/components/hooks/useInfinitePosts";
-import type { Post } from "@/types/post.interface";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { Link } from "react-router";
 
 const PostsPage = () => {
-  // const [posts, setPosts] = useState<Post[]>([]);
   const { fetchPosts, hasMore, loading, posts } = useInfinitePosts();
   const loaderRef = useRef(null);
 
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting && hasMore) {
+      if (entries[0].isIntersecting && hasMore && !loading) {
         fetchPosts();
       }
     });
 
     if (loaderRef.current) observer.observe(loaderRef.current);
     return () => observer.disconnect();
-  }, [hasMore]);
+  }, [hasMore, loading, fetchPosts]);
 
-  console.log(posts);
-
-  // Fetch Posts
-  // useEffect(() => {
-  //   const loadPosts = async () => {
-  //     const data = await getPosts();
-  //     if (data) setPosts(data);
-  //   };
-
-  //   loadPosts();
-  // }, [posts]);
+  console.log(posts, loading, hasMore);
 
   return (
     <main>
       <h1 className="text-3xl text-center my-4">Posts</h1>
       <ol className="flex flex-col gap-2 dark:bg-zinc-800 bg-blue-300">
-        {/* {posts.map(({ title, content, author, id }) => (
-          <Link to={`/posts/${id}`} key={id}>
-            <li className="p-4 flex flex-col gap-2 border-b-2">
-              <h1>
-                Title: <strong>{title}</strong>
-              </h1>
-              <p>{content}</p>
-              <p>Author: {author.name}</p>
-            </li>
-          </Link>
-        ))} */}
-        <div ref={loaderRef} className="h-20 flex items-center justify-center">
-          {loading && <p>Loading more posts...</p>}
-          {!hasMore && <p>You've reached the end!</p>}
-        </div>
+        {posts.length > 1 &&
+          posts.map(({ title, content, author, id }) => (
+            <Link to={`/posts/${id}`} key={id}>
+              <li className="p-4 flex flex-col gap-2 border-b-2">
+                <h1>
+                  Title: <strong>{title}</strong>
+                </h1>
+                <p>{content}</p>
+                <p>Author: {author.name}</p>
+              </li>
+            </Link>
+          ))}
       </ol>
+      <div ref={loaderRef} className="h-20 flex items-center justify-center">
+        {loading && <p>Loading more posts...</p>}
+        {!hasMore && <p>You've reached the end!</p>}
+      </div>
     </main>
   );
 };
