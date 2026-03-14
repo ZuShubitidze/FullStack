28cf783282b084a429eeae3a9cefc5cf1f555c6c
Zura Shubitidze <157276781+ZuShubitidze@users.noreply.github.com>
2026-03-14 14:54:16 +0400

    a

--- Backend/src/lib/prisma.ts
+++ Backend/src/lib/prisma.ts
@@ -1,18 +1,27 @@
-import "dotenv/config"; // Load .env variables
+// import "dotenv/config"; // Load .env variables
+// import { PrismaPg } from "@prisma/adapter-pg";
+// // import pg from "pg";
+// import { Pool } from "pg";
+// import { PrismaClient } from "../../generated/prisma/client.js";
+
+// // const { Pool } = pg;
+
+// // 1. Create the connection pool for PostgreSQL
+// const connectionString = process.env.DATABASE_URL;
+
+// const pool = new Pool({ connectionString });
+
+// // 2. Initialize the Prisma Adapter
+// const adapter = new PrismaPg(pool);
+
+// // 3. Create the Prisma Client instance
+// export const prisma = new PrismaClient({ adapter });
+
+import { PrismaClient } from "@generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
-// import pg from "pg";
-import { Pool } from "pg";
-import { PrismaClient } from "../../generated/prisma/client.js";
 
-// const { Pool } = pg;
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-// 1. Create the connection pool for PostgreSQL
-const connectionString = process.env.DATABASE_URL;
-
-const pool = new Pool({ connectionString });
-
-// 2. Initialize the Prisma Adapter
-const adapter = new PrismaPg(pool);
-
-// 3. Create the Prisma Client instance
 export const prisma = new PrismaClient({ adapter });
