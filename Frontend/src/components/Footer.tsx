f3197a4c4bdeca35d0575abc014d2cfd850347c3
Zura Shubitidze <157276781+ZuShubitidze@users.noreply.github.com>
2026-03-20 02:15:54 +0400

    Final update with Socket

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router";
+
 const Footer = () => {
-  return <footer className="my-auto">Footer</footer>;
+  return (
+    <footer className="my-auto flex justify-center items-center gap-10 pb-8">
+      <p>© {new Date().getFullYear()}</p>
+      <Link to={"/"}>Contact</Link>
+    </footer>
+  );
 };
 
 export default Footer;
--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -1,5 +1,51 @@
 const HomePage = () => {
-  return <main></main>;
+  return (
+    <main className="flex flex-col gap-4 md:gap-8">
+      <h1 className="text-3xl text-center">Welcome to my fullstack web app</h1>
+      <p>
+        Here, you can login / register, create posts with or without image,
+        comments and infinite replies to other comments. Viewing posts and
+        comments doesn't require registration.
+      </p>
+      <p>
+        You can also upload your profile picture, that will be displayed on your
+        posts next to your name as an author.
+      </p>
+      <p>
+        Using Socket, users also get notifications when someone comments on
+        their posts
+      </p>
+      <p>Backend is on Render, Frontend - on Vercel.</p>
+      <p>Data - Neon / Cloudinary</p>
+      <h1>Tools used:</h1>
+      <ul className="flex flex-row gap-20">
+        <section>
+          <h2>Backend</h2>
+          <li>Node.js</li>
+          <li>Express</li>
+          <li>Prisma</li>
+          <li>Bcrypt</li>
+          <li>JWT</li>
+          <li>Socket.io</li>
+        </section>
+        <section>
+          <h2>Frontend</h2>
+          <li>React / Vite</li>
+          <li>Tailwind / Shadcn</li>
+          <li>Tanstack / React Query</li>
+        </section>
+      </ul>
+      {/* Contact */}
+      <section className="flex flex-col gap-4">
+        <h3>Contact: </h3>
+        Name: Zura Shubitidze
+        <a href="mailto:zurashubitidze123@gmail.com">
+          Email: zurashubitidze123@gmail.com
+        </a>
+        <a href="tel:+558188548">Phone Number: 558188548</a>
+      </section>
+    </main>
+  );
 };
 
 export default HomePage;
