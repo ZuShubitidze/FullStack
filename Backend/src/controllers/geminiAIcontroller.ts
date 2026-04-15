effc33f199b153dd8333e74aafe00a10e107b2bd
Zura Shubitidze <157276781+ZuShubitidze@users.noreply.github.com>
2026-04-15 21:18:56 +0400

    Added Python

--- Backend/src/controllers/geminiAIcontroller.ts
+++ Backend/src/controllers/geminiAIcontroller.ts
@@ -6,11 +6,12 @@ const generateResponse = async (req: Request, res: Response) => {
   try {
     const { prompt } = req.body;
 
+    const PYTHON_SERVICE_URL =
+      process.env.PYTHON_SERVICE_URL || "http://127.0.0.1:8000";
+
     const pythonResponse = await axios.post(
-      "http://127.0.0.1:8000/geminiAI/generateResponse",
-      {
-        prompt: prompt,
-      },
+      `${PYTHON_SERVICE_URL}/geminiAI/generateResponse`,
+      { prompt },
     );
 
     // const response = await ai.models.generateContent({
@@ -22,9 +23,11 @@ const generateResponse = async (req: Request, res: Response) => {
     // });
 
     res.status(200).json(pythonResponse.data);
-  } catch (error) {
-    res.status(500).json({ message: "Error in controller" });
-    console.log("Error in geminiAI Controller");
+  } catch (error: any) {
+    console.error("Error in geminiAI Controller:", error.message);
+    res
+      .status(500)
+      .json({ message: "Error communicating with Python service" });
   }
 };
 
