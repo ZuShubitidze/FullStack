ad83d9d3723ce4124034ec2b65da90ece709260e
Zura Shubitidze <157276781+ZuShubitidze@users.noreply.github.com>
2026-04-18 14:50:52 +0400

    Removed Python

--- Backend/dist/src/server.js
+++ Backend/dist/src/server.js
@@ -1,5 +1,5 @@
-import "dotenv/config";
 import "module-alias/register";
+import "dotenv/config";
 import { prisma } from "./lib/prisma.js";
 import express from "express";
 import cookieParser from "cookie-parser";
