bd1853f29375db6246f0c03b36b3264e4a7c7951
Zura Shubitidze <157276781+ZuShubitidze@users.noreply.github.com>
2026-04-24 17:00:59 +0400

    Adding Generate Image

--- ai-service/src/routes/ai_routes.py
+++ ai-service/src/routes/ai_routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException
-from src.services.gemini import get_gemini_model, get_gemini_imagen_model, get_imagen_client
+# from src.services.gemini import get_gemini_model, get_gemini_imagen_model, get_imagen_client
+from src.services.gemini import chat_response, img_res, client
 import httpx
 
 router = APIRouter()
@@ -8,12 +9,17 @@ router = APIRouter()
 @router.post("/chat")
 async def chat_with_history(request: dict):
     try:
-        model = get_gemini_model()
+        # model = get_gemini_model()
         # Get Data from Request
         history = request.get("history") or []
         user_prompt = request.get("prompt")
         image_url = request.get("imageUrl")
-        chat = model.start_chat(history=history)
+
+        chat = client.chats.create(
+            model="gemini-2.0-flash",
+            history=history
+        )
+
         print(
             f"Received Prompt:{user_prompt}, ImageURL: {image_url}", flush=True)
 
@@ -22,17 +28,23 @@ async def chat_with_history(request: dict):
 
         if (image_url):
             print(f"FETCHING IMAGE: {image_url}", flush=True)
-            async with httpx.AsyncClient() as client:
+            async with httpx.AsyncClient() as imageClient:
                 # Get Image with httpx and Request URL
-                resp = await client.get(image_url)
+                resp = await imageClient.get(image_url)
                 content_type = resp.headers.get("Content-Type", "image/jpeg")
                 # Prepare multi-modal content
-                image_part = {"mime_type": content_type, "data": resp.content}
+                from google.genai import types
+                image_part = types.Part.from_bytes(
+                    data=resp.content,
+                    mime_type=content_type
+                )
+                # image_part = {"mime_type": content_type, "data": resp.content}
                 # Send message with both Image and Prompt
-                response = await chat.send_message_async([user_prompt, image_part])
+                # type: ignore
+                response = await chat.send_message([user_prompt, image_part])
         else:
             # If no Image, just send prompt
-            response = await chat.send_message_async(user_prompt, generation_config={})
+            response = await chat.send_message(user_prompt)  # type: ignore
 
         return {"reply": response.text}
     except Exception as e:
@@ -40,33 +52,33 @@ async def chat_with_history(request: dict):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.post("/generateImage")
-async def generate_image(request: str):
-    try:
-        client = get_imagen_client()
-        model = get_gemini_imagen_model(),
-        user_prompt = request
-        print(f"Received Prompt {user_prompt}", flush=True)
+# @router.post("/generateImage")
+# async def generate_image(request: str):
+#     try:
+#         client = get_imagen_client()
+#         model = get_gemini_imagen_model(),
+#         user_prompt = request
+#         print(f"Received Prompt {user_prompt}", flush=True)
 
-        response = client.models.generate_images(
-            model="imagen-3.0-generate-001",
-            prompt=user_prompt,
-        )
+#         response = client.models.generate_images(
+#             model="imagen-3.0-generate-001",
+#             prompt=user_prompt,
+#         )
 
-        if not response.generated_images or len(response.generated_images):
-            raise HTTPException(
-                status_code=400, detail="AI refused to generate image")
+#         if not response.generated_images or len(response.generated_images):
+#             raise HTTPException(
+#                 status_code=400, detail="AI refused to generate image")
 
-        image = response.generated_images[0]
+#         image = response.generated_images[0]
 
-        import base64
-        img_bytes = image.image_bytes  # type: ignore
-        image_base64 = base64.b64encode(img_bytes).decode('utf-8')
+#         import base64
+#         img_bytes = image.image_bytes  # type: ignore
+#         image_base64 = base64.b64encode(img_bytes).decode('utf-8')
 
-        return {
-            "reply": "Image generated successfully!",
-            "imageUrl": f"data:image/jpeg;base64,{image_base64}"
-        }
-    except Exception as e:
-        print(f"Imagen Error: {e}")
-        raise HTTPException(status_code=500, detail=str(e))
+#         return {
+#             "reply": "Image generated successfully!",
+#             "imageUrl": f"data:image/jpeg;base64,{image_base64}"
+#         }
+#     except Exception as e:
+#         print(f"Imagen Error: {e}")
+#         raise HTTPException(status_code=500, detail=str(e))
--- ai-service/src/services/gemini.py
+++ ai-service/src/services/gemini.py
@@ -1,6 +1,5 @@
 import os
-import google.generativeai as genai
-from google import genai as modern_genai
+from google import genai
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,18 +7,25 @@ api_key = os.getenv("GOOGLE_API_KEY")
 if not api_key:
     raise ValueError("GOOGLE_API_KEY not found in .env")
 # Setup Gemini
-genai.configure(api_key=api_key)  # type: ignore
-modern_client = modern_genai.Client(api_key=api_key)
+# genai.configure(api_key=api_key)
+client = genai.Client(api_key=api_key)
+
+# For Chat
+chat_response = client.models.generate_content(
+    model="gemini-2.0-flash", contents="Hello")
+
+# For Imagen
+img_res = client.models.generate_images(
+    model="imagen-3.0-generate-001", prompt="...")
 
 
-def get_gemini_model():
-    # type: ignore
-    return genai.GenerativeModel(model_name="gemini-2.5-flash-lite", system_instruction="", tools="")
+# def get_gemini_model():
+#     # return genai.GenerativeModel(model_name="gemini-2.5-flash-lite", system_instruction="", tools="")
 
 
-def get_imagen_client():
-    return modern_client
+# def get_imagen_client():
+#     # return modern_client
 
 
-def get_gemini_imagen_model():
-    return genai.GenerativeModel(model_name="imagen-4.0-generate-001")
+# def get_gemini_imagen_model():
+#     # return genai.GenerativeModel(model_name="imagen-4.0-generate-001")
