bbe5df14bf5c34660198fa1323d0b87c5818ffdf
Zura Shubitidze <157276781+ZuShubitidze@users.noreply.github.com>
2026-04-25 13:53:17 +0400

    Removed .venv

--- ai-service/src/routes/ai_routes.py
+++ ai-service/src/routes/ai_routes.py
@@ -6,49 +6,49 @@ import httpx
 router = APIRouter()
 
 
-@router.post("/chat")
-async def chat_with_history(request: dict):
-    try:
-        # model = get_gemini_model()
-        # Get Data from Request
-        history = request.get("history") or []
-        user_prompt = request.get("prompt")
-        image_url = request.get("imageUrl")
+# @router.post("/chat")
+# async def chat_with_history(request: dict):
+#     try:
+#         # model = get_gemini_model()
+#         # Get Data from Request
+#         history = request.get("history") or []
+#         user_prompt = request.get("prompt")
+#         image_url = request.get("imageUrl")
 
-        chat = client.chats.create(
-            model="gemini-2.0-flash",
-            history=history
-        )
+#         chat = client.chats.create(
+#             model="gemini-2.0-flash",
+#             history=history
+#         )
 
-        print(
-            f"Received Prompt:{user_prompt}, ImageURL: {image_url}", flush=True)
+#         print(
+#             f"Received Prompt:{user_prompt}, ImageURL: {image_url}", flush=True)
 
-        if not image_url:
-            print("DEBUG: No ImageURL found in request body", flush=True)
+#         if not image_url:
+#             print("DEBUG: No ImageURL found in request body", flush=True)
 
-        if (image_url):
-            print(f"FETCHING IMAGE: {image_url}", flush=True)
-            async with httpx.AsyncClient() as imageClient:
-                # Get Image with httpx and Request URL
-                resp = await imageClient.get(image_url)
-                content_type = resp.headers.get("Content-Type", "image/jpeg")
-                # Prepare multi-modal content
-                from google.genai import types
-                image_part = types.Part.from_bytes(
-                    data=resp.content,
-                    mime_type=content_type
-                )
-                # image_part = {"mime_type": content_type, "data": resp.content}
-                # Send message with both Image and Prompt
-                response = await chat.send_message([user_prompt, image_part])
-        else:
-            # If no Image, just send prompt
-            response = await chat.send_message(user_prompt)  # type: ignore
+#         if (image_url):
+#             print(f"FETCHING IMAGE: {image_url}", flush=True)
+#             async with httpx.AsyncClient() as imageClient:
+#                 # Get Image with httpx and Request URL
+#                 resp = await imageClient.get(image_url)
+#                 content_type = resp.headers.get("Content-Type", "image/jpeg")
+#                 # Prepare multi-modal content
+#                 from google.genai import types
+#                 image_part = types.Part.from_bytes(
+#                     data=resp.content,
+#                     mime_type=content_type
+#                 )
+#                 # image_part = {"mime_type": content_type, "data": resp.content}
+#                 # Send message with both Image and Prompt
+#                 response = await chat.send_message([user_prompt, image_part])
+#         else:
+#             # If no Image, just send prompt
+#             response = await chat.send_message(user_prompt)  # type: ignore
 
-        return {"reply": response.text}
-    except Exception as e:
-        print(f"Chat Error: {e}")
-        raise HTTPException(status_code=500, detail=str(e))
+#         return {"reply": response.text}
+#     except Exception as e:
+#         print(f"Chat Error: {e}")
+#         raise HTTPException(status_code=500, detail=str(e))
 
 
 # @router.post("/generateImage")
